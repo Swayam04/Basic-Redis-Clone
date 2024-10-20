9f595b00c85d49936923f46acfa5ddca98396a18
Swayamjeet <swayam.hp001@gmail.com>
2024-10-20 21:40:58 +0530

    Added multiple connection handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,22 +9,18 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
+        try(ServerSocket serverSocket = new ServerSocket(port);) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             while (true) {
-                clientSocket = serverSocket.accept();
+                Socket clientSocket = serverSocket.accept();
                 try {
                     if (clientSocket != null) {
                         OutputStream outputStream = clientSocket.getOutputStream();
                         outputStream.write("+PONG\r\n".getBytes());
-                        clientSocket.close();
                     }
                 } catch (IOException e) {
                     System.out.println("IOException: " + e.getMessage());
