9d6341ed7b0f2dadf7b6e6e91a7da08cad88f16b
Swayamjeet <swayam.hp001@gmail.com>
2024-12-22 18:29:16 +0530

    Added support for PSYNC in master mode

--- src/main/java/commands/CommandFactory.java
+++ src/main/java/commands/CommandFactory.java
@@ -18,6 +18,7 @@ public final class CommandFactory {
             case "keys" -> new KeysCommand(parsedCommand.name().toLowerCase(), parsedCommand.args());
             case "info" -> new InfoCommand(parsedCommand.name().toLowerCase(), parsedCommand.args());
             case "replconf" -> new ReplConfCommand(parsedCommand.name().toLowerCase(), parsedCommand.args());
+            case "psync" -> new PSyncCommand(parsedCommand.name().toLowerCase(), parsedCommand.args());
             default -> throw new UnsupportedOperationException("Unknown command: " + parsedCommand.name());
         };
     }
---
+++ src/main/java/commands/PSyncCommand.java
@@ -0,0 +1,39 @@
+package commands;
+
+import core.RedisServer;
+import resp.RespEncoder;
+
+import java.util.List;
+
+public class PSyncCommand extends RedisCommand {
+
+    public PSyncCommand(String name, List<String> args) {
+        super(name, args);
+    }
+
+    @Override
+    public String execute() {
+        String response = "FULLRESYNC " +
+                RedisServer.getReplicationInfo().getMasterReplId() +
+                " " +
+                RedisServer.getReplicationInfo().getMasterReplOffset();
+        return RespEncoder.encode(response, true);
+    }
+
+    @Override
+    public void checkSyntax() {
+        if (getArgs().size() < 2) {
+            throw new IllegalArgumentException("wrong number of arguments for " + name + " command");
+        }
+    }
+
+    @Override
+    public boolean isWriteCommand() {
+        return false;
+    }
+
+    @Override
+    public boolean isReplicaCommand() {
+        return true;
+    }
+}
